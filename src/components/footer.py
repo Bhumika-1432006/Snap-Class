6fa8c924ee677e9ec40b5e25dc6541ae9d36fefd
Bhumika-1432006 <guravbhumika808@gmail.com>
2026-06-11 14:24:06 +0530

    styling

--- src/components/footer.py
+++ src/components/footer.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 def footer_home():
-    logo_url = ""
+    logo_url = "https://assets.streamlinehq.com/image/private/w_200,h_200,ar_1/f_auto/v1/icons/kawaii-emoji/-symbols/-symbols/u+1f171-em1eez07mrbn93sx69c6c.png?_a=DATAiZAAZAA0"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
@@ -14,7 +14,7 @@ def footer_home():
 
 
 def footer_dashboard():
-    logo_url = ""
+    logo_url = "https://assets.streamlinehq.com/image/private/w_200,h_200,ar_1/f_auto/v1/icons/kawaii-emoji/-symbols/-symbols/u+1f171-em1eez07mrbn93sx69c6c.png?_a=DATAiZAAZAA0"
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
